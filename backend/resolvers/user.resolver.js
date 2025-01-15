00a6e53e16e5ee22df81256cac8313b3df6b1754
Alfonso Corona <coronaalfonsor@gmail.com>
2025-01-14 22:36:26 -0600

    feat: add mutations for user authentications

--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -1,15 +1,91 @@
 import { users } from '../dummyData/data.js'
+import User from '../models/user.model.js';
+import bcrypt from 'bcryptjs';
 
 const userResolver = {
-  Query: {
-    users: (_, { req, res }) => {
-      return users;
+  Mutation: {
+    signUp: async (_, {input}, context) => {
+      try {
+        const {username, name, email, password} = input;
+        if (!username || !name || !email || !password) {
+          throw new Error("All fields are required");
+        }
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+          throw new Error("User already exists");
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+        const user = new User({
+          username,
+          name,
+          password: hashedPassword,
+          gender,
+          profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+        });
+
+        await newUser.save();
+        await context.login(user);
+
+        return newUser;
+
+      }catch (error) {
+        console.error('Error in signUp resolver: ', error);
+        throw new Error(error.message || "An error occurred. Please try again.");
+      }
     },
-    user: (_, { userId }) => {
-      return users.find((user) => user._id === userId);
+    login: async (_, {input}, context) => {
+      try {
+        const {username, password} = input;
+        const {user} = await context.authenticate("graphql-local", {username, password});
+        await context.login(user);
+        return user;
+      } catch (error) {
+        console.error('Error in login resolver: ', error);
+        throw new Error(error.message || "An error occurred. Please try again.");
+      }
+    },
+    logout: async(_, __, context) => {
+      try {
+        await context.logout();
+        req.session.destroy((err) => {
+          if(err){
+            throw new Error("An error occurred", err);
+          }
+        });
+        res.clearCookie("connect.sid");
+        return {message: "Logged out successfully"};
+      } catch (error) {
+        console.log('Error in logout resolver: ', error);
+        throw new Error(error.message || "An error occurred. Please try again.");
+        
+      }
     }
   },
-  Mutation: {}
+  Query: {
+    authUser: async (_, __, context) => {
+      try {
+        const user = await context.getUser();
+        return user;
+      } catch (error) {
+        console.error('Error in authUser resolver: ', error);
+        throw new Error(error.message || "An error occurred. Please try again.");
+      }
+    },
+    user: async(_, { userId }) => {
+      try {
+        const user = await User.findById(userId);
+        return user;
+      } catch (error) {
+        console.error('Error in user resolver: ', error);
+        throw new Error(error.message || "An error occurred. Please try again.");
+      }
+    }
+  },
 }
 
 export default userResolver
