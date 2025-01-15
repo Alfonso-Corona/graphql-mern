b598e6c92e9a766e5b475e77fe22aa34e3f2353e
Alfonso Corona <coronaalfonsor@gmail.com>
2025-01-14 21:50:35 -0600

    feat: set passport authenticator

--- backend/index.js
+++ backend/index.js
@@ -3,20 +3,51 @@ import http from "http";
 import cors from "cors";
 import dotenv from "dotenv";
 import { connectDB } from "./db/connectDB.js";
+import passport from "passport";
+import session from "express-session";
+import connectMongo from "connect-mongodb-session";
+import { configurePassport } from "./passport/passport.config.js";
 
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { expressMiddleware } from "@apollo/server/express4";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { buildContext } from "graphql-passport";
 
 import mergedResolvers from "./resolvers/index.js";
 import mergedTypeDefs from "./typeDefs/index.js";
 
 dotenv.config();
+configurePassport();
 
 const app = express();
 
 const httpServer = http.createServer(app);
+
+const MongoDBStore = connectMongo(session);
+
+const store = new MongoDBStore({
+  url: process.env.MONGO_URI,
+  collection: "sessions",
+})
+
+store.on("error", (error) => console.error(error))
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      httpOnly: true,
+    },
+    store,
+  })
+)
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 const server = new ApolloServer({
   typeDefs: mergedTypeDefs,
@@ -27,10 +58,13 @@ const server = new ApolloServer({
 await server.start();
 
 app.use(
-  cors(),
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  }),
   express.json(),
   expressMiddleware(server, {
-    context: async ({ req }) => ({ req })
+    context: async ({ req, res }) => buildContext({ req, res })
   })
 )
 
--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -2,11 +2,11 @@ import { users } from '../dummyData/data.js'
 
 const userResolver = {
   Query: {
-    users: () => {
-      return users
+    users: (_, { req, res }) => {
+      return users;
     },
     user: (_, { userId }) => {
-      return users.find((user) => user._id === userId)
+      return users.find((user) => user._id === userId);
     }
   },
   Mutation: {}
